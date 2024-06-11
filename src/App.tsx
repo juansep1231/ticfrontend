9d2057f5df6434c3299b200e5ec751685662e6e8
VALERY ANTONELLA VALLEJO BARRIONUEVO <valery.vallejo@epn.edu.ec>
2024-06-11 03:34:13 +0000

    Merged PR 10: feat: ChakraUI setup
    1. ChakraUI installation
    2. Test ChakraUI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Box } from '@chakra-ui/react';
 
 const App: React.FC = () => {
-  return <div>Hola mundo!</div>;
+  return (
+    <Box
+      sx={{
+        fontSize: '32',
+        bg: 'blue',
+        textColor: 'white',
+      }}
+    >
+      Hola mundo!
+    </Box>
+  );
 };
 
 export default App;
