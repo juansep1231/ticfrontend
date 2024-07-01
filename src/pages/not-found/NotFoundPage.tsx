baa99beb7d64ed65f7858865ab616a38b2c61bc0
VALERY ANTONELLA VALLEJO BARRIONUEVO <valery.vallejo@epn.edu.ec>
2024-07-01 05:55:55 +0000

    Merged PR 20: refactor/not-found-page
    1. Add a 404 image.
    2. Refactor not found page.

--- src/pages/not-found/NotFoundPage.tsx
+++ src/pages/not-found/NotFoundPage.tsx
@@ -1,22 +1,49 @@
-import { Flex, Box, Heading } from '@chakra-ui/react';
+import { Flex, Heading, Image, Text } from '@chakra-ui/react';
 
 import { Navbar } from '../../components/Navbar';
 import { Footer } from '../../components/Footer';
 
 export const NotFoundPage = () => {
   return (
-    <Flex
-      sx={{
-        flexDirection: 'column',
-        minHeight: '100vh',
-        gap: 'xl',
-      }}
-    >
+    <Flex direction="column" minHeight="100vh">
       <Navbar />
-      <Box flex="1" sx={{ px: { base: 'md', lg: '3xl' } }}>
-        <Heading>404</Heading>
-        <Heading>NOT FOUND</Heading>
-      </Box>
+      <Flex
+        flex="1"
+        sx={{
+          px: { base: 'md', lg: '3xl' },
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Flex
+          sx={{
+            flexDirection: 'column',
+            gap: 'md',
+            alignItems: 'center',
+          }}
+        >
+          <Image
+            src="img/not-found.png"
+            alt="Not Found"
+            sx={{ width: '35%' }}
+          />
+          <Heading
+            sx={{ fontSize: 'heading.desktop.subtitle', color: 'brand.blue' }}
+          >
+            PÁGINA NO ENCONTRADA
+          </Heading>
+          <Text
+            sx={{
+              fontSize: 'text.lg',
+              color: 'text.default',
+              textAlign: 'center',
+            }}
+          >
+            Lo sentimos, la página que estás buscando no se encuentra disponible
+            o fue eliminada.
+          </Text>
+        </Flex>
+      </Flex>
       <Footer />
     </Flex>
   );
