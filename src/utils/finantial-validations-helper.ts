3a17674690cdc5d920a5c661b167bfec134b65e3
Valery Vallejo <valery.vallejo@epn.edu.ec>
2024-07-10 01:03:06 -0500

    chore: add account schema validation.

--- src/utils/finantial-validations-helper.ts
+++ src/utils/finantial-validations-helper.ts
@@ -1,5 +1,16 @@
 import * as yup from 'yup';
 
+export const accountSchema = yup.object().shape({
+  id: yup.number().optional(),
+  accountType: yup.string().required('El tipo de cuenta es obligatorio'),
+  accountName: yup.string().required('El nombre de la cuenta es obligatorio'),
+  currentValue: yup
+    .number()
+    .required('El valor actual es obligatorio')
+    .min(0, 'El valor actual no puede ser negativo'),
+  date: yup.string().required('La fecha es obligatoria'),
+});
+
 export const transactionSchema = yup.object().shape({
   date: yup.string().required('La fecha es obligatoria'),
   originAccount: yup
